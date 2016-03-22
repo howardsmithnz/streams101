bea5465b80800bc2f9f9c72725e05dc39c03e5f0
Howard Smith <howardsmithnz@gmail.com>
2016-03-22 15:21:21 +1300

    add another transform stream

--- streams_server_upper.js
+++ streams_server_upper.js
@@ -3,17 +3,23 @@ var fs = require('fs');
 var Transform = require('stream').Transform;
 
 var ts1 = new Transform;
+var ts2 = new Transform;
 
 ts1._transform = function(chunk, encoding, cb) {
-	// console.log("typeof chunk is: ", (typeof chunk))
-	// console.log(chunk)
+	// lets tranform the data stream by making everything uppercase
 	this.push(chunk.toString().toUpperCase());
+	cb()
+}
+
+ts2._transform = function(chunk, encoding, cb) {
+	// you could do another transform here...
+	this.push(chunk);
 	cb()
 }
 
 var server = http.createServer(function (req, res) {
     var stream = fs.createReadStream(__dirname + '/data.txt');
-    stream.pipe(ts1).pipe(res);
+    stream.pipe(ts2).pipe(ts1).pipe(res);
 });
 console.log("Starting streaming server...")
 server.listen(8000);
